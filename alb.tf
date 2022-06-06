dc6734c4e8812f7e962224c885a2fd03b6a1c218
peaceofanotherkind <39806827+peaceofanotherkind@users.noreply.github.com>
2022-06-06 17:22:07 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,31 +1,43 @@
-# Creating External LoadBalancer
-resource "aws_lb" "external-alb" {
-  name               = "External LB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.demosg.id]
-  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-1.id]
-}
-resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.demovpc.id
-}
-resource "aws_lb_target_group_attachment" "attachment" {
-  target_group_arn = aws_lb_target_group.external-alb.arn
-  target_id        = aws_instance.demoinstance.id
-  port             = 80
-depends_on = [
-    aws_instance.demoinstance,
-  ]
-}
-resource "aws_lb_listener" "external-elb" {
-  load_balancer_arn = aws_lb.external-alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.external-alb.arn
+# Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.demovpc.id
   }
+
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  security_group_id = [aws_security_group.demosg.id]
 }
+
+
+#ingress {
+#    description     = "Allow traffic from application layer"
+#    from_port       = 3306
+#    to_port         = 3306
+#    protocol        = "tcp"
+#    security_groups = [aws_security_group.demosg.id]
+#  }
+
+
+resource "aws_security_group_rule" "example" {
+  type              = "engress"
+  from_port         = 32768
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+#egress {
+#    from_port   = 32768
+#    to_port     = 65535
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+tags = {
+    Name = "Database SG"
+  }
