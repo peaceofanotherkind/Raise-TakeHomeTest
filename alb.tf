3e1f020faeaef8c72a61b74e9b08eff87807e452
peaceofanotherkind <39806827+peaceofanotherkind@users.noreply.github.com>
2022-06-06 17:30:11 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -13,31 +13,12 @@ resource "aws_security_group_rule" "example" {
   security_group_id = [aws_security_group.demosg.id]
 }
 
-
-#ingress {
-#    description     = "Allow traffic from application layer"
-#    from_port       = 3306
-#    to_port         = 3306
-#    protocol        = "tcp"
-#    security_groups = [aws_security_group.demosg.id]
-#  }
-
-
-resource "aws_security_group_rule" "example" {
-  type              = "engress"
-  from_port         = 32768
-  to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-#egress {
-#    from_port   = 32768
-#    to_port     = 65535
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-tags = {
-    Name = "Database SG"
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-alb.arn
   }
+}
